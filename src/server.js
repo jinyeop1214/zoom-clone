a52784ff02f69d29d80b8bd46764d79fc5f20fe0
jinyeop1214 <bb3598@gmail.com>
2023-03-19 15:12:26 +0900

    fe, be ws connection complete

--- src/server.js
+++ src/server.js
@@ -1,3 +1,7 @@
+//backend
+
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -8,6 +12,25 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const handleListen = () =>
+	console.log(`Listening on ws, http://localhost:3000`);
 
-app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose() {
+	console.log("Disconnected from the Browser.");
+}
+
+function onSocketMessage(message) {
+	console.log(message.toString("utf8"));
+}
+
+wss.on("connection", (socket) => {
+	console.log("Connected to Browser.");
+	socket.on("close", onSocketClose);
+	socket.on("message", onSocketMessage);
+	socket.send("hello!!!");
+});
+
+server.listen(3000, handleListen);
